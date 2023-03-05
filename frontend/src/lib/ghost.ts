69f9e16d4c57b402509d588299458cfa3d812279
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-05 21:34:42 +0900

    wip 原因がghostにあるので一旦保留

--- frontend/src/lib/ghost.ts
+++ frontend/src/lib/ghost.ts
@@ -2,7 +2,9 @@ import GhostContentAPI from "@tryghost/content-api";
 
 // Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
-  url: import.meta.env.GHOST_URL, // This is the default URL if your site is running on a local environment
-  key: import.meta.env.GHOST_CONTENT_KEY,
+  url: "https://demo.ghost.io",
+  key: "22444f78447824223cefc48062",
   version: "v5.0",
 });
+
+console.log(ghostClient);
