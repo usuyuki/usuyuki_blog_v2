47a8619ab02d134d55c06aaf41190b504bd71ebe
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-08 22:07:16 +0900

    feat 本番用は別で作る

--- frontend/src/lib/ghost.ts
+++ frontend/src/lib/ghost.ts
@@ -2,8 +2,8 @@ import GhostContentAPI from "@tryghost/content-api";
 
 // Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
-  url: "https://demo.ghost.io",
-  key: "22444f78447824223cefc48062",
+  url: import.meta.env.GHOST_CONTENT_KEY,
+  key: import.meta.env.GHOST_URL,
   version: "v5.0",
 });
 
