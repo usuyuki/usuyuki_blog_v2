a9bcc4fb816e487df761dcd8c0975abf39b980cf
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-09 00:45:00 +0900

    feat ちゃんと動く環境！

--- frontend/src/lib/ghost.ts
+++ frontend/src/lib/ghost.ts
@@ -2,8 +2,8 @@ import GhostContentAPI from "@tryghost/content-api";
 
 // Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
-  url: import.meta.env.GHOST_CONTENT_KEY,
-  key: import.meta.env.GHOST_URL,
+  url: import.meta.env.GHOST_URL,
+  key: import.meta.env.GHOST_CONTENT_KEY,
   version: "v5.0",
 });
 
