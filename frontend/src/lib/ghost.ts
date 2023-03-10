a9b6770bc47c4c12c1ee56d8e5dcdd3b7553806d
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-11 01:08:06 +0900

    feat .envでなく環境変数から値を入れるように変更

--- frontend/src/lib/ghost.ts
+++ frontend/src/lib/ghost.ts
@@ -2,7 +2,7 @@ import GhostContentAPI from '@tryghost/content-api';
 
 // Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
-  url: import.meta.env.GHOST_URL,
-  key: import.meta.env.GHOST_CONTENT_KEY,
+  url: process.env.GHOST_API_URL,
+  key: process.env.GHOST_CONTENT_KEY,
   version: 'v5.0',
 });
