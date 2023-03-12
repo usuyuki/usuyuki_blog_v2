e5fddad698a033f6256a3cec54fd0c94ae3ada49
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-13 00:15:44 +0900

    feat Image導入

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,24 +1,31 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import node from '@astrojs/node';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.usuyuki.net',
-	vite: {
-		resolve: {
-			alias: {
-				// tsconfig.jsonにも定義
-				'~': '/src'
-			}
-		}
-	},
-	integrations: [tailwind()],
-	server: { port: 1000, host: "0.0.0.0" },
-	output: 'server',
-	adapter: node({
-		// middlewareだとファイルをうまくできずFW作る必要あるのでstandaloneで動かす
-		mode: 'standalone'
-	})
+  site: 'https://blog.usuyuki.net',
+  vite: {
+    resolve: {
+      alias: {
+        // tsconfig.jsonにも定義
+        '~': '/src'
+      }
+    }
+  },
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }
+  )],
+  server: {
+    port: 1000,
+    host: "0.0.0.0"
+  },
+  output: 'server',
+  adapter: node({
+    // middlewareだとファイルをうまくできずFW作る必要あるのでstandaloneで動かす
+    mode: 'standalone'
+  })
 });
--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
+/// <reference types="@astrojs/image/client" />
 interface ImportMetaEnv {
 	readonly GHOST_CONTENT_KEY: string;
 	readonly GHOST_API_URL: string;
