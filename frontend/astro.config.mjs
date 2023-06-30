111d6e12fcce924692fd4afba8b1bb77810b2f75
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-06-30 12:13:18 +0900

    feat prerenderできるところはprerenderする

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,30 +2,33 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.usuyuki.net',
-  vite: {
-    resolve: {
-      alias: {
-        // tsconfig.jsonにも定義
-        '~': '/src'
-      }
-    }
-  },
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }
-  )],
-  server: {
-    port: 1000,
-    host: "0.0.0.0"
-  },
-  output: 'server',
-  adapter: node({
-    // middlewareだとファイルをうまくできずFW作る必要あるのでstandaloneで動かす
-    mode: 'standalone'
-  })
+	site: 'https://blog.usuyuki.net',
+	vite: {
+		resolve: {
+			alias: {
+				// tsconfig.jsonにも定義
+				'~': '/src'
+			}
+		}
+	},
+	integrations: [
+		tailwind(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp'
+		})
+	],
+	server: {
+		port: 1000,
+		host: '0.0.0.0'
+	},
+	// ほとんどのページが動的に変わるのでAstro2.6からのhybridは使わず従来のserverで変わらないところだけprerender=trueで対処
+	output: 'server',
+	adapter: node({
+		// middlewareだとファイルをうまくできずFW作る必要あるのでstandaloneで動かす
+		mode: 'standalone'
+	})
 });
