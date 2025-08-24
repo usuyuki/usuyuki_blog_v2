7d5f477f39421a33522c2e8c7173ae9fb794183b
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-08-24 22:58:15 +0900

    fix s抜け

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "うすゆきブログ";
-export const SITE_URL = "http://blog.usuyuki.net";
+export const SITE_URL = "https://blog.usuyuki.net";
 export const SITE_AUTHOR = "うすゆき";
 export const TWITTER_ID = "@usuyuki26";
 export const SITE_SUB_TITLE = "急須で入れたような何か";
