9adff8d6046b8e516b309703481727db2e2b5ef7
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-08-30 16:40:35 +0900

    fix import.metaだとbuildで決まってしまう

--- frontend/src/libs/imageProxy.ts
+++ frontend/src/libs/imageProxy.ts
@@ -1,8 +1,8 @@
 import { SITE_URL } from "~/consts";
 
 export function createProxyImageUrl(originalUrl: string): string {
-	const frontendUrl = import.meta.env.FRONTEND_URL || SITE_URL;
-	const ghostApiUrl = import.meta.env.GHOST_API_URL || "";
+	const frontendUrl = process.env.FRONTEND_URL || SITE_URL;
+	const ghostApiUrl = process.env.GHOST_API_URL || "";
 	const imageUrlPrefix = `${ghostApiUrl}/content/images/`;
 
 	if (!originalUrl || !ghostApiUrl || !originalUrl.startsWith(imageUrlPrefix)) {
